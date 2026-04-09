f4ecfe8c182e85d4a761c50b2bcf9d4c100c17ff
Aditya Tyagi <adityamcu1245@gmail.com>
2026-04-09 22:45:55 +0530

    basic code examples in java

---
+++ day1.java
@@ -0,0 +1,38 @@
+class Main {
+    public static void main(String[] args) {
+//          // int a='+';
+//          // // a='c';
+//          // System.out.println(a);
+         char ch='c';
+         ch=122;
+         System.out.println(ch);
+        
+        // int a=2;
+        // int b=15;
+        // int x = a+b;
+        // System.out.println(4156%10);
+        
+        
+//         write a program to print unit digit of a number 
+        // int x = 4156;
+        // System.out.print("The unit digit of " + x + " is " + x%10);
+        
+//         int x = 455;
+//         x=x+"2";
+//         System.out.print(x);
+        
+        // final int x = 5;   CANNOT BE CHANGED MADE CONSTANT
+        // x=x+5;
+        // System.out.print(x);
+        
+//         int x = 5;
+//         int y = x++;
+//         System.out.println(y);
+//         y=++x;
+//         System.out.println(y);
+        
+        int x = 10;
+        int y = ++x;
+        System.out.println(y==x);
+    }
+}
